5d885c29b47107bac3c8f8712ebf773621135732
downpour07 <choiheeyoung10@gmail.com>
2024-02-02 21:03:15 +0900

    ⚰️ :: login 기능 삭제

--- index.js
+++ index.js
@@ -76,52 +76,6 @@ app.post('/user/signup', async (req, res) => {
   }
 });
 
-//login
-app.post('/user/login', async (req, res) => {
-    const { accountId, password } = req.body;
-  
-    try {
-  
-      const thisUser = await User.findOne({ where: { accountId } });
-  
-      if(!thisUser) {
-  
-        return res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
-  
-      }
-  
-      const isPasswordValid = await bcrypt.compare(password, thisUser.password);
-  
-      if (!isPasswordValid) {
-  
-        return res.status(409).json({ message: "비밀번호가 일치하지 않습니다." });
-  
-      }
-  
-      //jwt 토큰 발급
-      const accessToken = jwt.sign(
-        { 
-          accountId: thisUser.accountId,
-        }, 
-        process.env.SECRET, 
-        {
-          expiresIn: "1h",
-      });
-  
-      await thisUser.update({
-        token: accessToken,
-      });
-  
-      res.status(201).json({ accessToken });
-  
-    } catch (error) {
-  
-      console.error(error);
-      res.status(500).json({ message: "서버 오류" });
-  
-    }
-});
-
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {

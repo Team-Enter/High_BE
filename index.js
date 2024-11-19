510c4be888a40bda1797fe937391f3d532449c18
downpour07 <choiheeyoung10@gmail.com>
2024-11-19 23:08:55 +0900

    🐛 :: 코드 삭제 복구

--- index.js
+++ index.js
@@ -10,30 +10,23 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   port: process.env.DB_PORT
 });
 
-//USER MODEL
 const User = sequelize.define('User', {
-  idx: { //고유번호
+  id: { //고유번호
     type: DataTypes.INTEGER,
     autoIncrement: true,
-    primaryKey: true,
+    primaryKey: true
   },
-  userid: { //아이디
+  accountId: { //아이디
     type: DataTypes.STRING,
-    unique: true,
+    unique: true
   },
-  userpw: { //비밀번호
+  nickname: { //닉네임
     type: DataTypes.STRING
   },
-  name: { //이름
+  password: { //비밀번호
     type: DataTypes.STRING
   },
-  state: { //병원 or 구급대원
-    type : DataTypes.STRING
-  },
-  phone: { //전화번호
-    type : DataTypes.STRING
-  },
-  connect: { //소속
+  email: { //이메일
     type: DataTypes.STRING
   },
   token: { //토큰
@@ -47,35 +40,39 @@ const Feed = sequelize.define('Feed', {
   id: { //고유번호
     type: DataTypes.INTEGER,
     autoIncrement: true,
-    primaryKey: true,
+    primaryKey: true
   },
-  phone: { //구급대원 전화번호
+  result: { //관련 분야
+    type: DataTypes.STRING
+  },
+  name: { //학교 이름
     type: DataTypes.STRING,
+    unique: true
   },
-  hospitalSelect: { //선택한 병원 이름
-    type: DataTypes.STRING,
+  Stype: { //학교 유형
+    type: DataTypes.STRING
   },
-  hospitalName: { //환자 이름
-    type: DataTypes.STRING,
+  location: { //학교 위치
+    type: DataTypes.STRING
   },
-  hospitalMonth: { //환자 나이
-    type : DataTypes.STRING,
+  phone: { //학교 번호
+    type: DataTypes.STRING
   },
-  hospitalBlood: { //환자 혈액형
-    type : DataTypes.STRING,
+  date: { //설립 일자
+    type: DataTypes.STRING
   },
-  hospitalInjury: { //환자 부상 부위
-    type: DataTypes.STRING,
+  Etype: { //설립 유형
+    type: DataTypes.STRING
   },
-  hospitalDisease: { //질병 여부
-    type: DataTypes.STRING,
+  gender: { //성별 구분
+    type: DataTypes.STRING
   },
-  hospitalSurgery: { //수술 여부
-    type: DataTypes.STRING,
+  link: { //홈페이지 링크
+    type: DataTypes.STRING
   },
-  state: { //병원 측 승인 여부 (승인: confm, 대기: wait)
-    type: DataTypes.STRING,
-  }
+  lesson: { //학교 학과
+    type: DataTypes.STRING
+  },
 }, {
   timestamps: false
 });
@@ -83,62 +80,76 @@ const Feed = sequelize.define('Feed', {
 const app = express();
 app.use(express.json());
 
+
 //user
+
 //회원가입
 app.post('/user/signup', async (req, res) => {
-  const { userid, userpw, name, state, phone, connect } = req.body;
+
+  const { accountId, nickname, password, email } = req.body;
+
+  console.log(req.body);
 
   try {
-    const thisUser = await User.findOne({ where: { userid } });
+
+    const thisUser = await User.findOne({ where: { accountId } });
 
     if (thisUser) {
-      return res.status(409).json({ message: "중복된 아이디입니다." });
+      return res.status(409).json({
+        message: "유저 아이디가 이미 존재합니다.",
+      });
     }
 
-    const hashedPassword = await bcrypt.hash(userpw, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    await User.create({ 
-        userid,
-        userpw: hashedPassword,
-        name,
-        state,
-        phone,
-        connect
+    const user = await User.create({ 
+        accountId: accountId,
+        nickname: nickname,
+        password: hashedPassword,
+        email: email
     });
 
-    return res.status(201).json("회원가입에 성공했습니다.");
+    res.status(201).json("성공적으로 자원을 생성하였습니다.");
+
   } catch (error) {
+
     console.error(error);
-    return res.status(400).json({ message: "요청에 실패했습니다." });
+    res.status(500).json({ message: "서버 오류" });
+
   }
 });
 
 //로그인
 app.post('/user/login', async (req, res) => {
-  const { userid, userpw } = req.body;
+  const { accountId, password } = req.body;
 
   try {
-    const thisUser = await User.findOne({ where: { userid } });
+
+    const thisUser = await User.findOne({ where: { accountId } });
 
     if(!thisUser) {
-      return res.status(404).json({ message: "존재하지 않는 아이디입니다." });
+
+      return res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+
     }
 
-    const isPasswordValid = await bcrypt.compare(userpw, thisUser.userpw);
+    const isPasswordValid = await bcrypt.compare(password, thisUser.password);
 
     if (!isPasswordValid) {
+
       return res.status(409).json({ message: "비밀번호가 일치하지 않습니다." });
+
     }
 
+    //jwt 토큰 발급
     const accessToken = jwt.sign(
       { 
-        userid: thisUser.userid,
+        accountId: thisUser.accountId,
       }, 
       process.env.SECRET, 
       {
         expiresIn: "1h",
-      }
-    );
+    });
 
     await thisUser.update({
       token: accessToken,
@@ -147,43 +158,10 @@ app.post('/user/login', async (req, res) => {
     res.status(201).json({ accessToken });
 
   } catch (error) {
+
     console.error(error);
-    res.status(400).json({ message: "요청에 실패했습니다." });
-  }
-});
+    res.status(500).json({ message: "서버 오류" });
 
-//유저 정보 불러오기
-app.get('/user/info', async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  try {
-
-    if (!authHeader) {
-      return res.status(401).json({ message: '토큰이 유효하지 않습니다.' });
-    }
-
-    const token = authHeader.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const userid = decodedToken.userid;
-
-    const thisUser = await User.findOne({ 
-      where: { userid },
-      attributes: { exclude: ["userpw", "token"] },
-    });
-
-    if (!thisUser) {
-      return res.status(404).json({ message: "유저를 찾을 수 없습니다." });
-    }
-
-    res.status(200).json({ 
-      name: thisUser.name,
-      phone: thisUser.phone,
-      connect: thisUser.connect
-    });
-
-  } catch (err) {
-    console.log(err);
-    return res.status(400).json({ message: "요청에 실패했습니다." });
   }
 });
 
@@ -194,197 +172,228 @@ app.post('/user/logout', async (req, res) => {
   try {
 
     if(!authHeader) {
-      return res.status(401).json({ message: "토큰이 유효하지 않습니다." });
+      return res.status(401).json({ message: "인증 토큰이 없습니다." });
     }
 
     const token = authHeader.split(' ')[1];
+
     const decodedToken = jwt.verify(token, process.env.SECRET);
-    const userid = decodedToken.userid;
-    const thisUser = await User.findOne({ where: { userid } });
+
+    const accountId = decodedToken.accountId;
+
+    const thisUser = await User.findOne({ where: { accountId } });
 
     if (!thisUser) {
-      return res.status(404).json({ message: "요청한 페이지를 찾을 수 없습니다." });
+      return res.status(404).json({
+        message: "요청한 페이지를 찾을 수 없습니다.",
+      });
     }
 
     await thisUser.update({
       token: null,
     });
 
-    return res.status(204).json({ message: "서버에서 정상적인 변경 또는 삭제 처리가 이루어졌지만, 새롭게 보일 정보가 없습니다." });
+    return res.status(204).json({
+      message: "서버에서 정상적인 변경 또는 삭제 처리가 이루어졌지만, 새롭게 보일 정보가 없습니다.",
+    });
     
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ message: "요청에 실패했습니다." });
-  }
-});
 
-//유저 정보 수정
-app.patch('/user', async (req, res) => {
-  const authHeader = req.headers.authorization;
-  const { name, phone, connect } = req.body;
-
-  try {
-
-    if(!authHeader) {
-      return res.status(401).json({ message: "토큰이 유효하지 않습니다." });
-    }
-
-    const token = authHeader.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const userid = decodedToken.userid;
-    const thisUser = await User.findOne({ where: { userid } });
-
-    if (!thisUser) {
-      return res.status(404).json({ message: "요청한 페이지를 찾을 수 없습니다." });
-    }
-
-    await thisUser.update({
-      name,
-      phone,
-      connect,
+    return res.status(500).json({
+      message: "서버 에러",
     });
 
-    return res.status(204).json({ message: "요청에 성공했습니다." });
-    
-  } catch (err) {
-    console.log(err);
-    return res.status(400).json({ message: "요청에 실패했습니다." });
   }
+
 });
 
-//회원탈퇴
-app.delete('/user', async (req, res) => {
+//사용자 정보 확인
+app.get('/user/info', async (req, res) => {
   const authHeader = req.headers.authorization;
 
   try {
 
-    if(!authHeader) {
-      return res.status(401).json({ message: "토큰이 유효하지 않습니다." });
+    if (!authHeader) {
+      return res.status(401).json({ message: '토큰이 유효하지 않습니다.' });
     }
 
     const token = authHeader.split(' ')[1];
+
     const decodedToken = jwt.verify(token, process.env.SECRET);
-    const userid = decodedToken.userid;
-    const thisUser = await User.findOne({ where: { userid } });
 
-    if (!thisUser) {
-      return res.status(404).json({ message: "요청한 페이지를 찾을 수 없습니다." });
-    }
+    const accountId = decodedToken.accountId;
 
-    await thisUser.destroy({
-      where: { userid: thisUser.userid },
+    const thisUser = await User.findOne({ 
+      where: { accountId },
+      attributes: { exclude: ["id", "password", "token"] },
     });
 
-    return res.status(204).json({ message: "서버에서 정상적인 변경 또는 삭제 처리가 이루어졌지만, 새롭게 보일 정보가 없습니다." });
+    if (!thisUser) {
+      return res.status(404).json({ message: "'유저를 찾을 수 없습니다.'" });
+    }
+
+    res.status(200).json({ 
+      accountId: thisUser.accountId,
+      nickname: thisUser.nickname,
+      email: thisUser.email
+    });
 
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ message: "요청에 실패했습니다." });
-  }
-});
 
-
-
-//feed
-//목록 불러오기
-app.post('/feed', async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  try {
-    if(!authHeader) {
-      return res.status(401).json({ message: "토큰이 유효하지 않습니다." });
-    }
-
-    const token = authHeader.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const userid = decodedToken.userid;
-    const thisUser = await User.findOne({ where: { userid } });
-
-    if (!thisUser) {
-      return res.status(404).json({ message: "요청한 페이지를 찾을 수 없습니다." });
-    }
-
-    const hospitalSelect = thisUser.connect;
-
-    const data = await Feed.findAll({ where: { hospitalSelect } });
-
-    return res.status(200).json(data);
-  } catch (err) {
-    console.error(err);
-    return res.status(400).json({ message: "요청에 실패했습니다." });
-  }
-});
-
-//상태 수정하기
-app.patch('/feed', async (req, res) => {
-  const authHeader = req.headers.authorization;
-  const { id, state } = req.body;
-
-  try {
-
-    if(!authHeader) {
-      return res.status(401).json({ message: "토큰이 유효하지 않습니다." });
-    }
-
-    const token = authHeader.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const userid = decodedToken.userid;
-    const thisUser = await User.findOne({ where: { userid } });
-
-    if (!thisUser) {
-      return res.status(404).json({ message: "요청한 페이지를 찾을 수 없습니다." });
-    }
-
-    const thisFeed = await User.findOne({ where: { id } });
-
-    await thisFeed.update({
-      state,
+    return res.status(500).json({
+      message: "서버 에러",
     });
 
-    return res.status(204).json({ message: "요청에 성공했습니다." });
-  } catch (err) {
-    console.log(err);
-    return res.status(400).json({ message: "요청에 실패했습니다." });
   }
+
 });
 
-//정보 등록하기
-app.post('/feed/post', async (req, res) => {
-  const authHeader = req.headers.authorization;
-  const { hospitalSelect, hospitalName, hospitalMonth, hospitalBlood, hospitalInjury, hospitalDisease, hospitalSurgery } = req.body;
+
+//feeds
+
+//고등학교 정보 추가
+app.post('/feeds/insert', async (req, res) => {
+  const { result, name, Stype, location, phone, date, Etype, gender, link, lesson } = req.body;
 
   try {
 
-    if(!authHeader) {
-      return res.status(401).json({ message: "토큰이 유효하지 않습니다." });
-    }
-
-    const token = authHeader.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const userid = decodedToken.userid;
-    const thisUser = await User.findOne({ where: { userid } });
-
-    if (!thisUser) {
-      return res.status(404).json({ message: "요청한 페이지를 찾을 수 없습니다." });
-    }
-
-    await Feed.create({ 
-      hospitalSelect,
-      hospitalName,
-      hospitalMonth,
-      hospitalBlood,
-      hospitalInjury,
-      hospitalDisease,
-      hospitalSurgery,
+    const feed = await Feed.create({ 
+      result: result,
+      name: name,
+      Stype: Stype,
+      location: location,
+      phone: phone,
+      date: date,
+      Etype: Etype,
+      gender: gender,
+      link: link,
+      lesson: lesson
     });
 
-    return res.status(201).json({ message: "요청에 성공했습니다." });
+    return res.status(200).json({
+      message: "입력 성공",
+    });
 
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ message: "요청에 실패했습니다." });
+
+    return res.status(500).json({
+      message: "서버 에러",
+    });
+
   }
+
+})
+
+
+//고등학교 추천
+app.get('/feeds', async (req, res) => {
+  const { firstresult, secondresult } = req.query;
+  
+  const authHeader = req.headers.authorization;
+
+  try {
+
+    if (!authHeader) {
+      return res.status(401).json({ message: '토큰이 유효하지 않습니다.' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+
+    const accountId = decodedToken.accountId;
+
+    const thisUser = await User.findOne({ where: { accountId } });
+
+    if (!thisUser) {
+      return res.status(404).json({ message: "유저를 찾을 수 없습니다." });
+    }
+
+    let result = firstresult;
+    const firstData = await Feed.findAll({ 
+      where: { result },
+      attributes: { exclude: [ "result", "id", "phone", "date", "Etype", "gender", "link", "lesson"] },
+    });
+
+    result = secondresult;
+    const secondData = await Feed.findAll({ 
+      where: { result },
+      attributes: { exclude: [ "result", "id", "phone", "date", "Etype", "gender", "link", "lesson"] },
+    });
+
+    res.status(200).json({
+      firstData,
+      secondData
+    });
+
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({
+      message: "서버 에러",
+    });
+
+  }
+
 });
+
+//고등학교 정보 조회
+app.get('/feeds/info', async (req, res) => {
+  const { name } = req.query;
+  
+  const authHeader = req.headers.authorization;
+
+  try {
+
+    if (!authHeader) {
+      return res.status(401).json({ message: '토큰이 유효하지 않습니다.' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+
+    const accountId = decodedToken.accountId;
+
+    const thisUser = await User.findOne({ where: { accountId } });
+
+    if (!thisUser) {
+      return res.status(404).json({ message: "유저를 찾을 수 없습니다." });
+    }
+
+    const data = await Feed.findOne({ 
+      where: { name },
+      attributes: { exclude: [ "id", "result" ] },
+    });
+
+    const lesson = data.lesson.split(',');
+
+    res.status(200).json({
+      "name": data.name,
+      "Stype": data.Stype,
+      "location": data.location,
+      "phone": data.phone,
+      "date": data.date,
+      "Etype": data.Etype,
+      "gender": data.gender,
+      "link": data.link,
+      "lesson": lesson
+    });
+
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({
+      message: "서버 에러",
+    });
+
+  }
+
+});
+
 
 const PORT = process.env.PORT || 8080;
 
